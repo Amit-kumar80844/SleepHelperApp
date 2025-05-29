8402e64af0de61365cdce02f395731111cdc2014
Amit-kumar80844 <156497411+Amit-kumar80844@users.noreply.github.com>
2025-05-29 22:23:28 +0530

    schedular screen to built

--- app/src/main/java/com/example/sleephelperapp/data/repository/SleepScheduleRepositoryImpl.kt
+++ app/src/main/java/com/example/sleephelperapp/data/repository/SleepScheduleRepositoryImpl.kt
@@ -8,21 +8,12 @@ class SleepRepository(private val dao: SleepScheduleDao) {
 
     val scheduleFlow: Flow<SleepScheduleEntity?> = dao.getSchedule()
 
-    suspend fun saveSchedule(schedule: SleepScheduleEntity) {
+    suspend fun saveOrUpdate(schedule: SleepScheduleEntity) {
         dao.insertSchedule(schedule)
     }
 
-    suspend fun updateToggle(key: String, value: Boolean, current: SleepScheduleEntity) {
-        val updated = when (key) {
-            "wakeUp" -> current.copy(wakeUpEnabled = value)
-            "sleepAlarm" -> current.copy(sleepAlarmEnabled = value)
-            "eyeComfort" -> current.copy(eyeComfortEnabled = value)
-            "notificationOff" -> current.copy(notificationOffEnabled = value)
-            "doNotDisturb" -> current.copy(doNotDisturbEnabled = value)
-            "flightMode" -> current.copy(flightModeEnabled = value)
-            "bwScreen" -> current.copy(blackAndWhiteEnabled = value)
-            else -> current
-        }
-        dao.updateSchedule(updated)
+    suspend fun update(schedule: SleepScheduleEntity) {
+        dao.updateSchedule(schedule)
     }
 }
+
